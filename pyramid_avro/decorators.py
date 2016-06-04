adb439ba6f1eff19c64f9af2a20dc5b66db92bb8
Alex Milstead <alex@amilstead.com>
2016-06-04 16:40:44 -0700

    Add decorator tests.

--- pyramid_avro/decorators.py
+++ pyramid_avro/decorators.py
@@ -6,12 +6,25 @@ class avro_message(p_view.view_config):
     def __call__(self, wrapped):
         settings = self.__dict__.copy()
         depth = settings.pop("_depth", 0)
+        settings.pop("message_impl", None)
 
         # Mostly a copy of view_config"s impl, just changing the config
         # attachment.
         def callback(context, name, ob):
+            if settings.get("service_name") is None:
+                # Try service_name attribute.
+                service_name = getattr(ob, "service_name", None)
+
+                # Else, try class name.
+                if service_name is None:
+                    service_name = ob.__name__.lower()
+
+                settings["service_name"] = service_name
+
+            message_name = settings.pop("message", None) or wrapped.__name__
+            settings["message"] = message_name
             config = context.config.with_package(info.module)
-            config.register_avro_message(message_impl=ob, **settings)
+            config.register_avro_message(message_impl=wrapped, **settings)
 
         info = self.venusian.attach(
             wrapped,
@@ -19,15 +32,6 @@ class avro_message(p_view.view_config):
             category="pyramid",
             depth=depth + 1
         )
-
-        if info.scope == "class":
-            # if the decorator was attached to a method in a class, or
-            # otherwise executed at class scope, we need to set an
-            # "attr" into the settings if one isn"t already in there
-            if settings.get("attr") is None:
-                settings["attr"] = wrapped.__name__
-
-        settings["_info"] = info.codeinfo # fbo "action_method"
         return wrapped
 
 
---
+++ tests/test_decorators.py
@@ -0,0 +1,85 @@
+import unittest
+import os
+
+from pyramid import config as p_config
+
+from pyramid_avro import decorators as pa_dec
+from pyramid_avro import routes as pa_routes
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+protocol_dir = os.path.join(here, "protocols")
+
+
+class Foo(object):
+
+    @pa_dec.avro_message()
+    def get(self, request):
+        pass
+
+    @pa_dec.avro_message(message="get2")
+    def get_other(self, request):
+        pass
+
+
+class Protocol(object):
+
+    service_name = "bar"
+
+    @pa_dec.avro_message()
+    def get(self, request):
+        pass
+
+    @pa_dec.avro_message(message="get2")
+    def get_other(self, request):
+        pass
+
+
+@pa_dec.avro_message(service_name="baz")
+def get(request):
+    pass
+
+
+class AvroMessageDecorator(unittest.TestCase):
+
+    def test_decorator_class(self):
+        settings = {
+            "avro.service.foo": "schema = protocols/test.avpr",
+            "avro.service.bar": "schema = protocols/test.avpr",
+            "avro.service.baz": "schema = protocols/test.avpr"
+        }
+        config = p_config.Configurator(settings=settings)
+        config.include("pyramid_avro")
+        config.scan()
+        config.commit()
+
+        # Test "foo"'s service route/registry.
+        foo_utility = config.registry.queryUtility(
+            pa_routes.IAvroServiceRoute,
+            name="avro.foo"
+        )
+        self.assertIsNotNone(foo_utility)
+        self.assertEqual(foo_utility.dispatch["get"], Foo.get.im_func)
+        self.assertEqual(foo_utility.dispatch["get2"], Foo.get_other.im_func)
+
+        # Test "bar"'s service route/registry.
+        bar_utility = config.registry.queryUtility(
+            pa_routes.IAvroServiceRoute,
+            name="avro.bar"
+        )
+        self.assertIsNotNone(bar_utility)
+        self.assertEqual(
+            bar_utility.dispatch["get"],
+            Protocol.get.im_func
+        )
+        self.assertEqual(
+            bar_utility.dispatch["get2"],
+            Protocol.get_other.im_func
+        )
+
+        # Test "baz"'s service route/registry.
+        baz_utility = config.registry.queryUtility(
+            pa_routes.IAvroServiceRoute,
+            name="avro.baz"
+        )
+        self.assertEqual(baz_utility.dispatch["get"], get)
